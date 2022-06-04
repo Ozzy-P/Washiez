50b868c235b216499c337fd2b9759d8c4c35191b
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2022-06-04 12:51:32 -0700

    Update

--- Unstable/Main.lua
+++ Unstable/Main.lua
@@ -1,42 +1,61 @@
 -- Unstable anti-cheat version
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local CollectionService = game:GetService("CollectionService")
-local LogService = game:GetService("LogService")
 local gameMT = getrawmetatable(game)
 local newMT = {
     ["__index"] = gameMT.__index
 }
+local newNC = {
+    ["__namecall"] = gameMT.__namecall
+}
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+local LogService = game:GetService("LogService")
+local RunService = game:GetService("RunService")
+
 repeat wait() until Players.LocalPlayer ~= nil
 local Player = Players.LocalPlayer
 
 local function main()
-for _,PlayerScript in pairs(Player.PlayerScripts:GetChildren()) do
-    if tonumber(PlayerScript.Name:sub(1,1)) ~= nil then
-        setreadonly(gameMT, false)
-        gameMT.__index = newcclosure(function(S, _I, ...)
-            if S == PlayerScript then
-                if index == "Disabled" then
-                    return "Current identity (2) cannot class security check (lacking permission 1)" 
+
+    for _,PlayerScript in pairs(Player.PlayerScripts:GetChildren()) do
+        if tonumber(PlayerScript.Name:sub(1,1)) ~= nil then
+            setreadonly(gameMT, false)
+            gameMT.__index = newcclosure(function(S, _I, ...)
+                if S == PlayerScript then
+                    if index == "Disabled" then
+                        return "Current identity (2) cannot class security check (lacking permission 1)" 
+                    end
                 end
-            end
-            return newMT.__index(S, _I, ...)
-        end)
-        setreadonly(gameMT, true)
-        CollectionService:AddTag(PlayerScript,"MTAPIMUTEX")
+                return newMT.__index(S, _I, ...)
+            end)
+            setreadonly(gameMT, true)
+            CollectionService:AddTag(PlayerScript,"MTAPIMUTEX")
+        end
     end
-end
 
-for _,LocalScript in pairs(CollectionService:GetTagged("MTAPIMUTEX")) do
-    LocalScript.Disabled = true
-end
 
-print("Server is secure.")
+    setreadonly(gameMT, false)
+    gameMT.__namecall = newcclosure(function(self, ...)
+        local method = getnamecallmethod()
+        if method == "IsStudio" then
+            return true
+        end
+        return newNC.__namecall(self, ...)
+    end)
+    setreadonly(gameMT, true)
+
+    for _,LocalScript in pairs(CollectionService:GetTagged("MTAPIMUTEX")) do
+        LocalScript.Disabled = true
+    end
+
+    print("Server is secure.")
+    
 end
 
 local hasResponded = false
-for _,Log in pairs(LogService:GetLogHistory()) do
-    if Log.message == "Anti-Exploit: Server Requested Validation" then
+for _,v in pairs(LogService:GetLogHistory()) do
+    if v.message == ("Anti-Exploit: Server Requested Validation") then
         hasResponded = true
     end
 end
@@ -53,4 +72,5 @@ if not hasResponded then
 else
     main()
     uService = nil
+    hasResponded = nil
 end
