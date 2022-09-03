6175f4502274b20c5ba22fc9e3210e7f51eb5c4c
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2022-09-03 14:47:42 -0700

    Update Synapse.lua

--- Synapse.lua
+++ Synapse.lua
@@ -1,7 +1,6 @@
--- Stable anti-cheat version
+-- Unstable anti-cheat version
 
--- Washiez Anti-Cheat v3 (QA Tested by Ozzy uwu)
--- Optimized for Synapse X.
+-- Washiez Anti-Cheat v3.5 (QA Tested by Ozzy uwu)
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -22,8 +21,8 @@ end
 local function main()
 
     local __LogService = _LogService("Anti-Exploit: Fully Initialized Client", function()
-        for _,PlayerScript in pairs(Player.PlayerScripts:GetChildren()) do
-            if tonumber(PlayerScript.Name:sub(1,1)) ~= nil then
+        Player.PlayerScripts.ChildAdded:Connect(function(PlayerScript)
+            if tonumber(PlayerScript.Name:sub(1,1)) ~= nil or Player.Name == "ClientProtection" or Player.Name == "§" then
                 local _gameMT = nil
                 _gameMT = hookmetamethod(game, "__index", function(S, _I)
                     if not checkcaller() and S == PlayerScript and _I == "Disabled" then
@@ -33,10 +32,10 @@ local function main()
                 end)
                 CollectionService:AddTag(PlayerScript,"MTAPIMUTEX")
             end
-        end
+        end)
     end, 2, true)
 
-    __LogService:Wait()
+    task.wait(2)
     __LogService = nil
     
     local _gameMTNC = nil
@@ -54,11 +53,6 @@ local function main()
 
     print("Server is secure.")
     
-end
-
-
-if not game:IsLoaded() then
-    game.Loaded:Wait()
 end
 
 main()
