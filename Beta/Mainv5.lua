4e9df606287bd58d880a9426920a7248bd5ed17a
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2022-10-27 21:38:52 -0700

    Update Mainv5.lua
    Formatting

--- Beta/Mainv5.lua
+++ Beta/Mainv5.lua
@@ -1,18 +1,15 @@
--- Washiez Anti-Cheat v5 (QA Tested by Ozzy uwu)
+-- Washiez Anti-Cheat v5 (WIP)
 -- Intercepts client after run time (use other version[s] for auto-execute)
 
-
-local function SendNotification(msg) --Self explanatory.
+local function SendNotification(msg)
 	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "Blume ctOS"; -- Old code from ctOS GUI, superseded by "Message" (FA ChBr.Dev)
+		Title = "Blume ctOS";
 		Text = msg;
 		Duration = 15;
 		Button1 = "Ok";
 	})
 end
 
--- Original by vinidalvino
--- https://v3rmillion.net/showthread.php?tid=992227
 local function get_fn_from_script()
     local found = {}
     for _,Env in pairs(debug.getregistry()) do
@@ -20,8 +17,8 @@ local function get_fn_from_script()
             pcall(function()
                 local newFenv = getfenv(Env)
                 if getfenv(Env).Kick or getfenv(Env).banMe then
-                    newFenv.Kick = function() print("UwU") end
-                    newFenv.banMe = function() print("UwU") end
+                    newFenv.Kick = function( end
+                    newFenv.banMe = function() end
                     setfenv(Env,newFenv)
                     table.insert(found,Env)
                 end
@@ -48,15 +45,15 @@ local old = mt.__namecall
 local protect = newcclosure or protect_function
 
 if not protect then
-protect = function(f) return f end
+    protect = function(f) return f end
 end
 
 setreadonly(mt, false)
 mt.__namecall = protect(function(self, ...)
-local method = getnamecallmethod()
-if method == "Kick" then
-wait(9e9)
-return
-end
-return old(self, ...)
+    local method = getnamecallmethod()
+    if method == "Kick" then
+        wait(9e9)
+        return
+    end
+    return old(self, ...)
 end)
